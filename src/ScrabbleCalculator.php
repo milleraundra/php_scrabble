6e1b1731ce3a8ed189f64efe7999148ff3db11bd
Aundra Miller and Brianna Popson <bpopson@gmail.com>
2016-02-18 09:05:39 -0800

    refactor point calculator

--- src/ScrabbleCalculator.php
+++ src/ScrabbleCalculator.php
@@ -6,13 +6,56 @@ class ScrabbleCalculator
     {
         $input_word = strtolower($input_word);
         $input_word = str_split($input_word);
+        $onePoint = array("a", "e" , "i" , "o", "u", "l", "n", "r", "s", "t");
+        $twoPoint = array("d", "g");
+        $threePoint = array("b", "c", "m", "p");
+        $fourPoint = array("f", "h", "v", "w", "p");
+        $fivePoint = array("k");
+        $eightPoint = array("j", "x");
+        $tenPoint = array("q", "z");
+        
         $totalScore = 0;
 
         foreach ($input_word as $letter){
-            if ($letter == "a" || "e" || "i" || "o" || "u" || "l" || "n" || "r" || "s" || "t"){
-                $totalScore += 1;
+            foreach ($onePoint as $index => $alphabet) {
+                if ($letter == $onePoint[$index]) {
+                    $totalScore += 1;
+                }
+            }
+            foreach ($twoPoint as $index => $alphabet) {
+                if ($letter == $twoPoint[$index]) {
+                    $totalScore += 2;
+                }
+            }
+
+            foreach ($threePoint as $index => $alphabet) {
+                if ($letter == $threePoint[$index]) {
+                    $totalScore += 3;
+                }
+            }
+
+            foreach ($fourPoint as $index => $alphabet) {
+                if ($letter == $fourPoint[$index]) {
+                    $totalScore += 4;
+                }
+            }
+            foreach ($fivePoint as $index => $alphabet) {
+                if ($letter == $fivePoint[$index]) {
+                    $totalScore += 5;
+                }
+            }
+            foreach ($eightPoint as $index => $alphabet) {
+                if ($letter == $eightPoint[$index]) {
+                    $totalScore += 8;
+                }
+            }
+            foreach ($tenPoint as $index => $alphabet) {
+                if ($letter == $tenPoint[$index]) {
+                    $totalScore += 10;
+                }
             }
         }
+
         return $totalScore;
 
     }
--- tests/ScrabbleCalculatorTest.php
+++ tests/ScrabbleCalculatorTest.php
@@ -7,7 +7,6 @@
 
         function test_short_word()
         {
-
             //Arrange
             $test_ScrabbleCalculator = new ScrabbleCalculator;
             $scrabbleWord = "it";
@@ -17,6 +16,32 @@
 
             //Assert
             $this->assertEquals(2, $result);
+        }
+
+        function test_slightly_bigger_word()
+        {
+            //Arrange
+            $test_ScrabbleCalculator = new ScrabbleCalculator;
+            $scrabbleWord = "dog";
+
+            //Act
+            $result = $test_ScrabbleCalculator->pointCalculator($scrabbleWord);
+
+            //Assert
+            $this->assertEquals(5, $result);
+        }
+
+        function test_big_word()
+        {
+            //Arrange
+            $test_ScrabbleCalculator = new ScrabbleCalculator;
+            $scrabbleWord = "adbfkjq";
+
+            //Act
+            $result = $test_ScrabbleCalculator->pointCalculator($scrabbleWord);
+
+            //Assert
+            $this->assertEquals(33, $result);
         }
 
     }
